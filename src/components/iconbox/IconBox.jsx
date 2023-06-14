2b3019f7134fd66d0f7ab8d91e1f6b42c3fca656
MuhammadWahaj075 <wahajsiddiqui07@gmail.com>
2023-06-14 16:08:43 +0500

    minor changes

--- src/components/iconbox/IconBox.jsx
+++ src/components/iconbox/IconBox.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 const IconBox = () => {
   return (
-    <div class="row-iconbox">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
+    <div className="row-iconbox">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="60"
               data-mobile="60"
               data-smobile="60"
             ></div>
-            <div class="themesflat-headings style-1 text-center clearfix">
-              <h2 class="heading">YOUR BEST CHOOSE</h2>
-              <div class="sep has-icon width-125 clearfix">
-                <div class="sep-icon">
-                  <span class="sep-icon-before sep-center sep-solid"></span>
-                  <span class="icon-wrap">
-                    <i class="Hammi-Architecture-icon-build"></i>
+            <div className="themesflat-headings style-1 text-center clearfix">
+              <h2 className="heading">YOUR BEST CHOOSE</h2>
+              <div className="sep has-icon width-125 clearfix">
+                <div className="sep-icon">
+                  <span className="sep-icon-before sep-center sep-solid"></span>
+                  <span className="icon-wrap">
+                    <i className="Hammi-Architecture-icon-build"></i>
                   </span>
-                  <span class="sep-icon-after sep-center sep-solid"></span>
+                  <span className="sep-icon-after sep-center sep-solid"></span>
                 </div>
               </div>
-              <p class="sub-heading">
+              <p className="sub-heading">
                 Elevate your space with our visionary designs that blend
                 creativity, functionality, and timeless elegance.
               </p>
             </div>
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="42"
               data-mobile="35"
               data-smobile="35"
@@ -37,23 +37,23 @@ const IconBox = () => {
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-md-4">
+        <div className="row">
+          <div className="col-md-4">
             <div
-              class="themesflat-content-box clearfix"
+              className="themesflat-content-box clearfix"
               data-margin="0 5px 0 5px"
               data-mobilemargin="0 0 0 0"
             >
-              <div class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                <div class="icon-wrap">
-                  <i class="Hammi-Architecture-icon-quality"></i>
+              <div className="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                <div className="icon-wrap">
+                  <i className="Hammi-Architecture-icon-quality"></i>
                 </div>
-                <div class="text-wrap">
-                  <h5 class="heading">
+                <div className="text-wrap">
+                  <h5 className="heading">
                     <a href="#">BEST QUALITY</a>
                   </h5>
-                  <div class="sep clearfix"></div>
-                  <p class="sub-heading">
+                  <div className="sep clearfix"></div>
+                  <p className="sub-heading">
                     Hammi-Architecture Construction Services are committed to
                     meeting the highest quality standards without compromising
                     our safety culture..
@@ -62,28 +62,28 @@ const IconBox = () => {
               </div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div className="col-md-4">
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="0"
               data-mobile="0"
               data-smobile="35"
             ></div>
             <div
-              class="themesflat-content-box clearfix"
+              className="themesflat-content-box clearfix"
               data-margin="0 5px 0 5px"
               data-mobilemargin="0 0 0 0"
             >
-              <div class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                <div class="icon-wrap">
-                  <i class="Hammi-Architecture-icon-time"></i>
+              <div className="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                <div className="icon-wrap">
+                  <i className="Hammi-Architecture-icon-time"></i>
                 </div>
-                <div class="text-wrap">
-                  <h5 class="heading">
+                <div className="text-wrap">
+                  <h5 className="heading">
                     <a href="#">ON TIME</a>
                   </h5>
-                  <div class="sep clearfix"></div>
-                  <p class="sub-heading">
+                  <div className="sep clearfix"></div>
+                  <p className="sub-heading">
                     At our company, we respect the customer’s time and schedule
                     and always complete the projects on timely fashion way.
                   </p>
@@ -91,28 +91,28 @@ const IconBox = () => {
               </div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div className="col-md-4">
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="0"
               data-mobile="0"
               data-smobile="35"
             ></div>
             <div
-              class="themesflat-content-box clearfix"
+              className="themesflat-content-box clearfix"
               data-margin="0 5px 0 5px"
               data-mobilemargin="0 0 0 0"
             >
-              <div class="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
-                <div class="icon-wrap">
-                  <i class="Hammi-Architecture-icon-author"></i>
+              <div className="themesflat-icon-box icon-top align-center has-width w95 circle light-bg accent-color style-1 clearfix">
+                <div className="icon-wrap">
+                  <i className="Hammi-Architecture-icon-author"></i>
                 </div>
-                <div class="text-wrap">
-                  <h5 class="heading">
+                <div className="text-wrap">
+                  <h5 className="heading">
                     <a href="#">EXPERIENCED</a>
                   </h5>
-                  <div class="sep clearfix"></div>
-                  <p class="sub-heading">
+                  <div className="sep clearfix"></div>
+                  <p className="sub-heading">
                     As the saying goes practice makes perfect. With our years of
                     experience you can bet on us to get the job done exactly to
                     your specifications.
@@ -123,21 +123,21 @@ const IconBox = () => {
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-md-12">
+        <div className="row">
+          <div className="col-md-12">
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="41"
               data-mobile="35"
               data-smobile="35"
             ></div>
-            <div class="elm-button text-center">
-              <a href="#" class="themesflat-button bg-accent">
+            <div className="elm-button text-center">
+              <a href="#" className="themesflat-button bg-accent">
                 ABOUT US
               </a>
             </div>
             <div
-              class="themesflat-spacer clearfix"
+              className="themesflat-spacer clearfix"
               data-desktop="73"
               data-mobile="60"
               data-smobile="60"
